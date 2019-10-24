d7001849a4e553ff0d5f2ebc385d6d594a77c64f
ali najmaldin <ali.najmaldin@gmail.com>
2019-10-24 14:06:22 -0400

    Finisih Question 1 added question 2

--- question1.py
+++ question1.py
@@ -1,6 +1,9 @@
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy import stats
 
 class RandomWalk:
     step = ['up', 'down', 'right', 'left']
@@ -24,27 +27,43 @@ class RandomWalk:
         previousX, previousY = self.positions[currentStep-1,]
         if nextStep == 'up':
             newX, newY = previousX, previousY + 1
-        elif nextStep == 'down':
+        if nextStep == 'down':
             newX, newY = previousX, previousY - 1
-        elif nextStep == 'right':
+        if nextStep == 'right':
             newX, newY = previousX + 1, previousY
-        elif nextStep == 'left':
-            newX, newY = previousX - 1, previousY +1
+        if nextStep == 'left':
+            newX, newY = previousX - 1, previousY
         self.positions[currentStep,] = (newX, newY)
 
-    def EuclidianDistance(self):
+    def EuclidianDistanceLast(self):
         dist = np.linalg.norm(self.positions[0,]- self.positions[self.Nsteps -1,])
         return dist
 
     def averageEuclidianDistance(self):
-        meanDist = np.linalg.norm(self.positions[0,] - self.positions)
-        return meanDist/self.Nsteps
+        meanDist = 0
+        for i in range(self.Nsteps):
+            meanDist = meanDist + np.linalg.norm((self.positions[0,]) - self.positions[i,])
+        return meanDist/(self.Nsteps-1)
 
     def printer(self):
         print(self.positions)
 
-walker = RandomWalk((0,0), 1000)
-walker.fullWalk()
-# walker.printer()
-print(walker.EuclidianDistance())
-print(walker.averageEuclidianDistance())
+
+distributionLastPoint = np.zeros(1000)
+distributionAverageDist = np.zeros(1000)
+for i in range(1000):
+    walker = RandomWalk((0,0), 1000)
+    walker.fullWalk()
+    distributionLastPoint[i] = walker.EuclidianDistanceLast()
+    distributionAverageDist[i] = walker.averageEuclidianDistance()
+
+
+f, axes = plt.subplots(2, 1)
+# f.title('Main title')
+sns.distplot(distributionLastPoint, bins=20, kde=False, ax=axes[0]);
+axes[0].set_title('Distribution of Euclidian Distances of the Last Point')
+axes[1].set_title('Distribution of Average Euclidian Distances')
+
+
+sns.distplot(distributionAverageDist, bins=20, kde=False, ax=axes[1]);
+plt.show()
